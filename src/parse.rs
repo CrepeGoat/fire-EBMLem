e59ee32706737365fb007501906390c5ba4382fd
CrepeGoat <studentoflife96@gmail.com>
2021-02-15 13:38:29 -0500

    merged in "basic-ebml-parser"

---
+++ src/parse.rs
@@ -0,0 +1,346 @@
+use std::cmp::min;
+use std::mem::size_of;
+use std::ops::RangeFrom;
+
+use nom::{
+    bits::streaming::take as take_bits, bytes::streaming::take as take_bytes, error::ParseError,
+    Err, IResult, InputIter, InputLength, Needed, Slice, ToUsize,
+};
+
+fn take_rem<I, E: ParseError<(I, usize)>>(
+) -> impl Fn((I, usize)) -> IResult<(I, usize), (u8, usize), E>
+where
+    I: Slice<RangeFrom<usize>> + InputIter<Item = u8> + InputLength,
+{
+    move |(input, bit_offset): (I, usize)| {
+        let bitlen = (8usize - bit_offset) % 8usize;
+        take_bits(bitlen)((input, bit_offset))
+            .map(move |((input, bit_offset), bits)| ((input, bit_offset), (bits, bitlen)))
+    }
+}
+
+fn take_zeros<I, C, E: ParseError<(I, usize)>>(
+    max_count: C,
+) -> impl Fn((I, usize)) -> IResult<(I, usize), usize, E>
+where
+    I: Slice<RangeFrom<usize>> + InputIter<Item = u8> + InputLength,
+    C: ToUsize,
+{
+    let max_count = max_count.to_usize();
+    move |(mut input, bit_offset): (I, usize)| {
+        if max_count == 0 {
+            return Ok(((input, bit_offset), 0usize));
+        }
+
+        let mut streak_len: usize = 0;
+        let mut item = input
+            .iter_elements()
+            .next()
+            .ok_or_else(|| Err::Incomplete(Needed::new(1)))?;
+        item &= 0xFF >> bit_offset; // mask out first `bit_offset` bits
+
+        streak_len += (item.leading_zeros() as usize) - bit_offset;
+        while item.leading_zeros() == 8 && streak_len <= max_count {
+            input = input.slice(1..);
+            if streak_len == max_count {
+                break;
+            };
+            item = input
+                .iter_elements()
+                .next()
+                .ok_or_else(|| Err::Incomplete(Needed::new(1)))?;
+            streak_len += item.leading_zeros() as usize;
+        }
+        streak_len = min(streak_len, max_count);
+
+        Ok(((input, (streak_len + bit_offset) % 8), streak_len))
+    }
+}
+
+macro_rules! make_vlen_parser {
+    ($func_name:ident, $uint:ty) => {
+        fn $func_name(input: &[u8]) -> IResult<&[u8], $uint, ()> {
+            // Parse length from stream
+            let ((input, bit_offset), len) = take_zeros(size_of::<$uint>())((input, 0))?;
+            if len >= size_of::<$uint>() {
+                return Err(nom::Err::Error(()));
+            }
+            let ((input, bit_offset), _) = take_bits::<_, usize, _, ()>(1u8)((input, bit_offset))?;
+            let ((input, _), (leftover_bits, _)) = take_rem()((input, bit_offset))?;
+            let (input, bytes) = take_bytes(len)(input)?;
+
+            let mut buffer = [0u8; size_of::<$uint>()];
+            buffer[size_of::<$uint>() - len - 1] = leftover_bits;
+            buffer[(size_of::<$uint>() - len)..].copy_from_slice(bytes);
+
+            Ok((input, <$uint>::from_be_bytes(buffer)))
+        }
+    };
+}
+
+make_vlen_parser!(vlen_to_u32, u32);
+make_vlen_parser!(vlen_to_u64, u64);
+
+const RESERVED_ELEMENT_ID: u32 = u32::MAX;
+
+pub fn element_id(input: &[u8]) -> IResult<&[u8], u32, ()> {
+    let (new_input, result) = vlen_to_u32(input)?;
+
+    let len = unsafe { new_input.as_ptr().offset_from(input.as_ptr()) } as usize;
+    if result == 0 || result.count_ones() == 7 * (len as u32) {
+        // if all non-length bits are 0's or 1's
+        // corner-case: reserved ID's
+        return Ok((new_input, RESERVED_ELEMENT_ID));
+    }
+    if (8 * size_of::<u32>() - (result.leading_zeros() as usize)) >= 7 * (len - 1) {
+        // element ID's must use the smallest representation possible
+        return Err(nom::Err::Error(()));
+    }
+
+    Ok((new_input, result))
+}
+
+const UNKNOWN_ELEMENT_LEN: u64 = u64::MAX;
+
+pub fn element_len(input: &[u8]) -> IResult<&[u8], u64, ()> {
+    let (new_input, result) = vlen_to_u64(input)?;
+
+    let len = unsafe { new_input.as_ptr().offset_from(input.as_ptr()) };
+    Ok(if result.count_ones() == 7 * (len as u32) {
+        // if all non-length bits are 1's
+        // corner-case: reserved ID's
+        (new_input, UNKNOWN_ELEMENT_LEN)
+    } else {
+        (new_input, result)
+    })
+}
+
+fn parse_length<'a>(input: &'a [u8], buffer: &mut [u8]) -> IResult<&'a [u8], (), ()> {
+    let (input, bytes) = take_bytes(buffer.len())(input)?;
+    buffer.copy_from_slice(bytes);
+
+    Ok((input, ()))
+}
+
+pub fn uint(input: &[u8], length: usize) -> IResult<&[u8], u64, ()> {
+    assert!(
+        length <= size_of::<u64>(),
+        format!(
+            "invalid length for uint (expected n<{:?}, found {:?})",
+            size_of::<u64>(),
+            length,
+        )
+    );
+
+    let mut buffer = [0u8; size_of::<u64>()];
+    let i0 = size_of::<i64>() - length;
+    let (input, _) = parse_length(input, &mut buffer[i0..])?;
+
+    Ok((input, u64::from_be_bytes(buffer)))
+}
+
+pub fn int(input: &[u8], length: usize) -> IResult<&[u8], i64, ()> {
+    assert!(
+        length <= size_of::<i64>(),
+        format!(
+            "invalid length for int (expected n<{:?}, found {:?})",
+            size_of::<i64>(),
+            length,
+        )
+    );
+
+    let buffer_fill: u8 = match take_bits(1usize)((input, 0))? {
+        ((_, 1), 0) => 0x00,
+        ((_, 1), 1) => 0xFF,
+        _ => unreachable!(),
+    };
+    let mut buffer = [buffer_fill; size_of::<i64>()];
+    let i0 = size_of::<i64>() - length;
+    let (input, _) = parse_length(input, &mut buffer[i0..])?;
+
+    Ok((input, i64::from_be_bytes(buffer)))
+}
+
+pub fn float32(input: &[u8], length: usize) -> IResult<&[u8], f32, ()> {
+    assert!(
+        length == size_of::<f32>(),
+        format!(
+            "invalid length for f32 (expected {:?}, found {:?})",
+            size_of::<f32>(),
+            length,
+        )
+    );
+
+    let mut buffer = [0u8; size_of::<f32>()];
+    let (input, _) = parse_length(input, &mut buffer)?;
+
+    Ok((input, f32::from_be_bytes(buffer)))
+}
+
+pub fn float64(input: &[u8], length: usize) -> IResult<&[u8], f64, ()> {
+    assert!(
+        length == size_of::<f64>(),
+        format!(
+            "invalid length for f64 (expected {:?}, found {:?})",
+            size_of::<f64>(),
+            length,
+        )
+    );
+
+    let mut buffer = [0u8; size_of::<f64>()];
+    let (input, _) = parse_length(input, &mut buffer)?;
+
+    Ok((input, f64::from_be_bytes(buffer)))
+}
+
+pub fn ascii_str(input: &[u8], length: usize) -> IResult<&[u8], &str, ()> {
+    let (input, result) = unicode_str(input, length)?;
+    if !result[..].is_ascii() {
+        return Err(nom::Err::Error(()));
+    }
+
+    Ok((input, result))
+}
+
+pub fn unicode_str(input: &[u8], length: usize) -> IResult<&[u8], &str, ()> {
+    let (input, bytes) = take_bytes(length)(input)?;
+    let result = std::str::from_utf8(bytes).or(Err(nom::Err::Error(())))?;
+
+    Ok((input, result))
+}
+
+pub fn date(input: &[u8], length: usize) -> IResult<&[u8], i64, ()> {
+    assert!(
+        length == size_of::<i64>(),
+        format!(
+            "invalid length for timestamp (expected {:?}, found {:?})",
+            size_of::<i64>(),
+            length,
+        )
+    );
+
+    int(input, length)
+}
+
+pub fn binary(input: &[u8], length: usize) -> IResult<&[u8], &[u8], ()> {
+    take_bytes(length)(input)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use rstest::*;
+
+    #[rstest(source, bit_offset, expt_result,
+        case(
+            &[0b_0100_1010, 0b_1010_0101], 3,
+            ((&[0b_1010_0101][..], 0), (0b0_1010_u8, 5)),
+        ),
+        case(
+            &[0b_0100_1010, 0b_1010_0101], 0,
+            ((&[0b_0100_1010, 0b_1010_0101][..], 0), (0u8, 0)),
+        ),
+    )]
+    fn test_take_rem(
+        source: &'static [u8],
+        bit_offset: usize,
+        expt_result: ((&'static [u8], usize), (u8, usize)),
+    ) {
+        assert_eq!(take_rem::<_, ()>()((source, bit_offset)), Ok(expt_result),);
+    }
+
+    #[rstest(source, bit_offset, max_count, expt_result,
+        case(
+            &[0b_0000_0000, 0b_0100_1010], 3, usize::MAX,
+            ((&[0b_0100_1010][..], 1), 6),
+        ),
+        case(
+            &[0b_1110_0000, 0b_0100_1010], 3, usize::MAX,
+            ((&[0b_0100_1010][..], 1), 6),
+        ),
+        case(
+            &[0b_0000_0000, 0b_0100_1010], 3, 5,
+            ((&[0b_0100_1010][..], 0), 5),
+        ),
+    )]
+    fn test_take_zeros(
+        source: &'static [u8],
+        bit_offset: usize,
+        max_count: usize,
+        expt_result: ((&'static [u8], usize), usize),
+    ) {
+        assert_eq!(
+            take_zeros::<_, _, ()>(max_count)((source, bit_offset)),
+            Ok(expt_result),
+        );
+    }
+
+    #[test]
+    fn test_element_id() {
+        let source = [0x40, 0x01, 0xFF];
+        assert_eq!(element_id(&source[..]), Ok((&source[2..], 1)));
+    }
+
+    #[test]
+    fn test_element_len() {
+        let source = [0x40, 0x01, 0xFF];
+        assert_eq!(element_len(&source[..]), Ok((&source[2..], 1)));
+    }
+
+    #[test]
+    fn test_uint() {
+        let source = [0x40, 0x01, 0xFF];
+        assert_eq!(uint(&source[..], 1), Ok((&source[1..], source[0] as u64)));
+    }
+
+    #[test]
+    fn test_int() {
+        let source = [0x40, 0x01, 0xFF];
+        assert_eq!(
+            int(&source[..], 1),
+            Ok((&source[1..], i8::from_be_bytes([source[0]]) as i64))
+        );
+    }
+
+    #[test]
+    fn test_float32() {
+        let num = 3.0f32;
+        let source = num.to_be_bytes();
+        assert_eq!(float32(&source[..], 4), Ok((&source[4..], num)));
+    }
+
+    #[test]
+    fn test_float64() {
+        let num = 5.0f64;
+        let source = num.to_be_bytes();
+        assert_eq!(float64(&source[..], 8), Ok((&source[8..], num)));
+    }
+
+    #[test]
+    fn test_ascii_str() {
+        let source = b"I am a string, I am only a string.";
+        assert_eq!(ascii_str(&source[..], 8), Ok((&source[8..], "I am a s")));
+    }
+
+    #[test]
+    fn test_unicode_str() {
+        let s = "知ら ない の か ？ 死神 の 霊 絡 は 色 が 違う って こと ｡";
+        let source = s.as_bytes();
+        assert_eq!(
+            unicode_str(&source[..], 25),
+            Ok((&source[25..], "知ら ない の か ？"))
+        );
+    }
+
+    #[test]
+    fn test_date() {
+        let source = [0x40, 0x01, 0xFF, 0x00, 0x40, 0x01, 0xFF, 0x00, 0xFF, 0xFF];
+        assert_eq!(
+            date(&source[..], 8),
+            Ok((
+                &source[8..],
+                i64::from_be_bytes([0x40, 0x01, 0xFF, 0x00, 0x40, 0x01, 0xFF, 0x00],)
+            )),
+        );
+    }
+}
