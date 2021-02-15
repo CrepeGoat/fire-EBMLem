3e00d1a5cda22330c57f49f75df33d363f6b8163
CrepeGoat <awqatty.b@gmail.com>
2021-02-15 01:25:11 -0500

    fixup

--- src/write.rs
+++ src/write.rs
@@ -1,32 +1,27 @@
 use std::cmp::min;
 use std::mem::size_of;
 
-use nom::{
-    Err, IResult, Needed,
-};
+use nom::{Err, IResult, Needed};
 
-
-fn give_bits((output, bit_offset): (&mut [u8], usize), (source, length): (u8, usize)) -> IResult<(&mut [u8], usize), (), ()> {
+fn give_bits(
+    (output, bit_offset): (&mut [u8], usize),
+    (source, length): (u8, usize),
+) -> IResult<(&mut [u8], usize), (), ()> {
     if length == 0 {
         return Ok(((output, bit_offset), ()));
     }
 
     let size_rem = 8 - bit_offset;
-    let right_offset = size_rem - length;
-    if right_offset < 0 {
-        return Err(nom::Err::Error(()));
-    }
+    let right_offset = size_rem.checked_sub(length).ok_or(nom::Err::Error(()))?;
 
     let bitmask = (!(0xFFu8 >> length)) >> bit_offset;
     output[0] = (output[0] & !bitmask) | ((source << right_offset) & bitmask);
 
-    Ok(
-        if right_offset == 0 {
-            ((&mut output[1..], 0), ())
-        } else {
-            ((output, bit_offset + length), ())
-        }
-    )
+    Ok(if right_offset == 0 {
+        ((&mut output[1..], 0), ())
+    } else {
+        ((output, bit_offset + length), ())
+    })
 }
 
 fn give_bytes<'a>(output: &'a mut [u8], source: &[u8]) -> IResult<&'a mut [u8], (), ()> {
@@ -38,9 +33,12 @@ fn give_bytes<'a>(output: &'a mut [u8], source: &[u8]) -> IResult<&'a mut [u8], 
     Ok((&mut output[source.len()..], ()))
 }
 
-
-
-fn vlen_int(output: &mut [u8], value: u64, min_length: Option<usize>, max_length: Option<usize>) -> IResult<&mut [u8], usize, ()> {
+fn vlen_int(
+    output: &mut [u8],
+    value: u64,
+    min_length: Option<usize>,
+    max_length: Option<usize>,
+) -> IResult<&mut [u8], usize, ()> {
     //let bitlen = 8*size_of::<u64>() - (value.leading_zeros() as usize);
     let mut vint_len = {
         if value < 0x0000_0000_0000_0080 {
@@ -74,13 +72,13 @@ fn vlen_int(output: &mut [u8], value: u64, min_length: Option<usize>, max_length
     }
 
     let bit_offset = 0;
-    let ((output, bit_offset), _) = give_bits((output, bit_offset), (0, vint_len-1))?;
+    let ((output, bit_offset), _) = give_bits((output, bit_offset), (0, vint_len - 1))?;
     let ((output, bit_offset), _) = give_bits((output, bit_offset), (1, 1))?;
 
     let source = value.to_be_bytes();
     let ((output, bit_offset), _) = give_bits(
         (output, bit_offset),
-        (source[size_of::<u64>() - vint_len], 8 - bit_offset)
+        (source[size_of::<u64>() - vint_len], 8 - bit_offset),
     )?;
     assert_eq!(bit_offset, 0); // -> safe to operate on the byte-level
     let (output, _) = give_bytes(output, &source[size_of::<u64>() - (vint_len - 1)..])?;
@@ -101,8 +99,10 @@ fn element_id(output: &mut [u8], value: u64) -> IResult<&mut [u8], usize, ()> {
     )
 }
 
-
-fn element_len(output: &mut [u8], value: u64, bytelen: Option<usize>) -> IResult<&mut [u8], usize, ()> {
+fn element_len(
+    output: &mut [u8],
+    value: u64,
+    bytelen: Option<usize>,
+) -> IResult<&mut [u8], usize, ()> {
     vlen_int(output, value, bytelen, Some(8))
 }
-
