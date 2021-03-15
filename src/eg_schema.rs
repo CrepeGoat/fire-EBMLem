5e1eef425b65ab43a3ff5e91b7d767e137a6c0a2
CrepeGoat <awqatty.b@gmail.com>
2021-03-14 21:03:01 -0400

    tmp: implemented `StreamState` for all body elements

--- src/eg_schema.rs
+++ src/eg_schema.rs
@@ -549,7 +549,51 @@ impl Files {
     }
 }
 
-impl StreamState for Files {}
+impl StreamState for Files {
+    fn next<'a>(&mut self, stream: &'a [u8]) -> nom::IResult<&'a [u8], bool, ()> {
+        match self {
+            Self(_, ElementParsingStage::Start) | Self(_, ElementParsingStage::Interlude) => {
+                let orig_stream = stream;
+
+                let (stream, id) = parse::element_id(stream)?;
+                let (stream, child_elem) = match id {
+                    File::ID => {
+                        let (stream, e) = File::init_from_stream(stream)?;
+                        Ok((
+                            stream,
+                            ElementParsingStage::Child(Files_SubElements::File_Variant(e)),
+                        ))
+                    }
+                    _ => Err(nom::Err::Failure(())),
+                }?;
+
+                self.0 -= stream_diff(orig_stream, stream);
+                self.1 = child_elem;
+
+                Ok((stream, false))
+            }
+            Self(_, ElementParsingStage::Finish) | Self(_, ElementParsingStage::EndOfStream) => {
+                Ok((stream, true))
+            }
+            Self(length_rem, ElementParsingStage::Child(variant)) => {
+                let (stream, ended) = match variant {
+                    Files_SubElements::File_Variant(e) => e.next(stream),
+                }?;
+
+                if ended {
+                    self.1 = if *length_rem == 0 {
+                        ElementParsingStage::Finish
+                    } else {
+                        ElementParsingStage::Interlude
+                    };
+                }
+
+                Ok((stream, false))
+            }
+            Self(_length_rem, ElementParsingStage::Global(_global, _child)) => todo!(),
+        }
+    }
+}
 
 impl Element for Files {
     const ID: u32 = 0x1946696C;
@@ -587,7 +631,90 @@ struct File(
     ElementParsingStage<<Self as MasterElement>::SubElements, <Self as MasterElement>::SubGlobals>,
 );
 
-impl StreamState for File {}
+impl File {
+    fn init_from_stream(stream: &[u8]) -> nom::IResult<&[u8], Self, ()> {
+        let (stream, len) = parse::element_len(stream)?;
+        match len {
+            ElementLength::Known(len) => {
+                let len: usize = len.try_into().unwrap();
+                Ok((stream, Self(len, ElementParsingStage::Start)))
+            }
+            ElementLength::Unknown => Err(nom::Err::Failure(())),
+        }
+    }
+}
+
+impl StreamState for File {
+    fn next<'a>(&mut self, stream: &'a [u8]) -> nom::IResult<&'a [u8], bool, ()> {
+        match self {
+            Self(_, ElementParsingStage::Start) | Self(_, ElementParsingStage::Interlude) => {
+                let orig_stream = stream;
+
+                let (stream, id) = parse::element_id(stream)?;
+                let (stream, child_elem) = match id {
+                    FileName::ID => {
+                        let (stream, e) = FileName::init_from_stream(stream)?;
+                        Ok((
+                            stream,
+                            ElementParsingStage::Child(File_SubElements::FileName_Variant(e)),
+                        ))
+                    }
+                    MimeType::ID => {
+                        let (stream, e) = MimeType::init_from_stream(stream)?;
+                        Ok((
+                            stream,
+                            ElementParsingStage::Child(File_SubElements::MimeType_Variant(e)),
+                        ))
+                    }
+                    ModificationTimestamp::ID => {
+                        let (stream, e) = ModificationTimestamp::init_from_stream(stream)?;
+                        Ok((
+                            stream,
+                            ElementParsingStage::Child(
+                                File_SubElements::ModificationTimestamp_Variant(e),
+                            ),
+                        ))
+                    }
+                    Data::ID => {
+                        let (stream, e) = Data::init_from_stream(stream)?;
+                        Ok((
+                            stream,
+                            ElementParsingStage::Child(File_SubElements::Data_Variant(e)),
+                        ))
+                    }
+                    _ => Err(nom::Err::Failure(())),
+                }?;
+
+                self.0 -= stream_diff(orig_stream, stream);
+                self.1 = child_elem;
+
+                Ok((stream, false))
+            }
+            Self(_, ElementParsingStage::Finish) | Self(_, ElementParsingStage::EndOfStream) => {
+                Ok((stream, true))
+            }
+            Self(length_rem, ElementParsingStage::Child(variant)) => {
+                let (stream, ended) = match variant {
+                    File_SubElements::FileName_Variant(e) => e.next(stream),
+                    File_SubElements::MimeType_Variant(e) => e.next(stream),
+                    File_SubElements::ModificationTimestamp_Variant(e) => e.next(stream),
+                    File_SubElements::Data_Variant(e) => e.next(stream),
+                }?;
+
+                if ended {
+                    self.1 = if *length_rem == 0 {
+                        ElementParsingStage::Finish
+                    } else {
+                        ElementParsingStage::Interlude
+                    };
+                }
+
+                Ok((stream, false))
+            }
+            Self(_length_rem, ElementParsingStage::Global(_global, _child)) => todo!(),
+        }
+    }
+}
 
 impl Element for File {
     const ID: u32 = 0x6146;
@@ -617,7 +744,13 @@ enum File_SubElements {
 }
 
 #[derive(Debug, Clone)]
-struct FileName {}
+struct FileName;
+
+impl FileName {
+    fn init_from_stream(stream: &[u8]) -> nom::IResult<&[u8], Self, ()> {
+        Ok((stream, Self))
+    }
+}
 
 impl StreamState for FileName {}
 
@@ -637,7 +770,13 @@ impl UTF8Element for FileName {
 }
 
 #[derive(Debug, Clone)]
-struct MimeType {}
+struct MimeType;
+
+impl MimeType {
+    fn init_from_stream(stream: &[u8]) -> nom::IResult<&[u8], Self, ()> {
+        Ok((stream, Self))
+    }
+}
 
 impl StreamState for MimeType {}
 
@@ -657,7 +796,13 @@ impl StringElement for MimeType {
 }
 
 #[derive(Debug, Clone)]
-struct ModificationTimestamp {}
+struct ModificationTimestamp;
+
+impl ModificationTimestamp {
+    fn init_from_stream(stream: &[u8]) -> nom::IResult<&[u8], Self, ()> {
+        Ok((stream, Self))
+    }
+}
 
 impl StreamState for ModificationTimestamp {}
 
@@ -678,7 +823,13 @@ impl DateElement for ModificationTimestamp {
 }
 
 #[derive(Debug, Clone)]
-struct Data {}
+struct Data;
+
+impl Data {
+    fn init_from_stream(stream: &[u8]) -> nom::IResult<&[u8], Self, ()> {
+        Ok((stream, Self))
+    }
+}
 
 impl StreamState for Data {}
 
