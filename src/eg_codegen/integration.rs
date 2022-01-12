53b4be05b61cdfecfe1c573fe782d4329f3b4d8e
CrepeGoat <studentoflife96@gmail.com>
2022-01-11 21:55:07 -0500

    added integration test to find instances of a given element and collect the values thereof

--- src/eg_codegen/integration.rs
+++ src/eg_codegen/integration.rs
@@ -1,70 +1,70 @@
 use crate::eg_codegen::parser;
 
+const BYTE_STREAM: [u8; 146] = [
+    // ### Files 1 ###
+    0x19, 0x46, 0x69, 0x6C, // Files element ID
+    0xDA, // Files length = 90
+    //
+    // --- File 1 ---
+    0x61, 0x46, // File element ID
+    0xAB, // File length = 43
+    0x61, 0x4E, // FileName element ID
+    0x8A, // FileName length = 10
+    0x66, 0x69, 0x6c, 0x65, 0x33, 0x2e, 0x68, 0x74, 0x6d,
+    0x6c, // FileName data = "file3.html"
+    0x46, 0x4D, // MimeType element ID
+    0x89, // MimeType length = 9
+    0x74, 0x65, 0x78, 0x74, 0x2f, 0x68, 0x74, 0x6d, 0x6c, // MimeType data = "text/html"
+    0x46, 0x54, // ModificationTimestamp element ID
+    0x88, // ModificationTimestamp length
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // ModificationTimestamp data = 0
+    0x46, 0x64, // Data element ID
+    0x84, // Data length = 4
+    0x01, 0x02, 0x03, 0x04, // Data data
+    //
+    // --- File 2 ---
+    0x61, 0x46, // File element ID
+    0xA9, // File length = 41
+    0x46, 0x54, // ModificationTimestamp element ID
+    0x88, // ModificationTimestamp length
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // ModificationTimestamp data = 0
+    0x46, 0x64, // Data element ID
+    0x84, // Data length = 4
+    0x01, 0x02, 0x03, 0x04, // Data data
+    0x46, 0x4D, // MimeType element ID
+    0x88, // MimeType length = 8
+    0x74, 0x65, 0x78, 0x74, 0x2f, 0x63, 0x73, 0x76, // MimeType data = "text/csv"
+    0x61, 0x4E, // FileName element ID
+    0x89, // FileName length = 9
+    0x66, 0x69, 0x6c, 0x65, 0x32, 0x2e, 0x63, 0x73, 0x76, // FileName data = "file2.csv"
+    //
+    // ### Files 2 ###
+    0x19, 0x46, 0x69, 0x6C, // Files element ID
+    0xAE, // Files length = 46
+    //
+    // --- File 1 ---
+    0x61, 0x46, // File element ID
+    0xAB, // File length = 43
+    0x61, 0x4E, // FileName element ID
+    0x89, // FileName length = 9
+    0x66, 0x69, 0x6c, 0x65, 0x31, 0x2e, 0x74, 0x78, 0x74, // FileName data = "file1.txt"
+    0x46, 0x4D, // MimeType element ID
+    0x8A, // MimeType length = 10
+    0x74, 0x65, 0x78, 0x74, 0x2f, 0x70, 0x6c, 0x61, 0x69,
+    0x6e, // MimeType data = "text/plain"
+    0x46, 0x54, // ModificationTimestamp element ID
+    0x88, // ModificationTimestamp length
+    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // ModificationTimestamp data = 0
+    0x46, 0x64, // Data element ID
+    0x84, // Data length = 4
+    0x01, 0x02, 0x03, 0x04, // Data data
+];
+
 #[test]
 fn basic_traversal() {
-    let byte_stream = [
-        // ### Files 1 ###
-        0x19, 0x46, 0x69, 0x6C, // Files element ID
-        0xDA, // Files length = 90
-        //
-        // --- File 1 ---
-        0x61, 0x46, // File element ID
-        0xAB, // File length = 43
-        0x61, 0x4E, // FileName element ID
-        0x8A, // FileName length = 10
-        0x66, 0x69, 0x6c, 0x65, 0x33, 0x2e, 0x68, 0x74, 0x6d,
-        0x6c, // FileName data = "file3.html"
-        0x46, 0x4D, // MimeType element ID
-        0x89, // MimeType length = 9
-        0x74, 0x65, 0x78, 0x74, 0x2f, 0x68, 0x74, 0x6d, 0x6c, // MimeType data = "text/html"
-        0x46, 0x54, // ModificationTimestamp element ID
-        0x88, // ModificationTimestamp length
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // ModificationTimestamp data = 0
-        0x46, 0x64, // Data element ID
-        0x84, // Data length = 4
-        0x01, 0x02, 0x03, 0x04, // Data data
-        //
-        // --- File 2 ---
-        0x61, 0x46, // File element ID
-        0xA9, // File length = 41
-        0x46, 0x54, // ModificationTimestamp element ID
-        0x88, // ModificationTimestamp length
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // ModificationTimestamp data = 0
-        0x46, 0x64, // Data element ID
-        0x84, // Data length = 4
-        0x01, 0x02, 0x03, 0x04, // Data data
-        0x46, 0x4D, // MimeType element ID
-        0x88, // MimeType length = 8
-        0x74, 0x65, 0x78, 0x74, 0x2f, 0x63, 0x73, 0x76, // MimeType data = "text/csv"
-        0x61, 0x4E, // FileName element ID
-        0x89, // FileName length = 9
-        0x66, 0x69, 0x6c, 0x65, 0x31, 0x2e, 0x63, 0x73, 0x76, // FileName data = "file2.csv"
-        //
-        // ### Files 2 ###
-        0x19, 0x46, 0x69, 0x6C, // Files element ID
-        0xAE, // Files length = 46
-        //
-        // --- File 1 ---
-        0x61, 0x46, // File element ID
-        0xAB, // File length = 43
-        0x61, 0x4E, // FileName element ID
-        0x89, // FileName length = 9
-        0x66, 0x69, 0x6c, 0x65, 0x31, 0x2e, 0x74, 0x78, 0x74, // FileName data = "file1.txt"
-        0x46, 0x4D, // MimeType element ID
-        0x8A, // MimeType length = 10
-        0x74, 0x65, 0x78, 0x74, 0x2f, 0x70, 0x6c, 0x61, 0x69,
-        0x6e, // MimeType data = "text/plain"
-        0x46, 0x54, // ModificationTimestamp element ID
-        0x88, // ModificationTimestamp length
-        0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, // ModificationTimestamp data = 0
-        0x46, 0x64, // Data element ID
-        0x84, // Data length = 4
-        0x01, 0x02, 0x03, 0x04, // Data data
-    ];
-
     let mut reader: parser::Readers<_> = parser::_DocumentReader::new(
-        &byte_stream[..],
-        parser::_DocumentState::new(byte_stream.len()),
+        &BYTE_STREAM[..],
+        parser::_DocumentState::new(BYTE_STREAM.len()),
     )
     .into();
     let mut result = Vec::new();
@@ -100,6 +100,45 @@ fn basic_traversal() {
             "Data", "File", "Files", "File", "ModTime", "File", "Data", "File", "MimeType", "File",
             "FileName", "File", "Files", "(None)", "Files", "File", "FileName", "File", "MimeType",
             "File", "ModTime", "File", "Data", "File", "Files", "(None)",
+        ]
+    );
+}
+
+#[test]
+fn find_all_element_instances() {
+    let mut reader: parser::Readers<_> = parser::_DocumentReader::new(
+        &BYTE_STREAM[..],
+        parser::_DocumentState::new(BYTE_STREAM.len()),
+    )
+    .into();
+    let mut result = Vec::new();
+
+    // FilesDef
+    // FileDef
+    // FileName
+
+    loop {
+        reader = match reader {
+            parser::Readers::_Document(r) => r.next().unwrap().into(),
+            parser::Readers::Files(r) => r.next().unwrap().into(),
+            parser::Readers::File(r) => r.next().unwrap().into(),
+            parser::Readers::FileName(mut r) => {
+                result.push(r.read().unwrap().to_string());
+                r.next().unwrap().into()
+            }
+            parser::Readers::MimeType(r) => r.skip().unwrap().into(),
+            parser::Readers::ModificationTimestamp(r) => r.skip().unwrap().into(),
+            parser::Readers::Data(r) => r.skip().unwrap().into(),
+            parser::Readers::None(_) => break,
+        };
+    }
+
+    assert_eq!(
+        result,
+        vec![
+            "file3.html".to_string(),
+            "file2.csv".to_string(),
+            "file1.txt".to_string()
         ]
     );
 }
